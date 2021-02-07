2758d5bbb5949bd20dd4c0a3f6170fdee1136995
Piotr Glodek <pioglodek@gmail.com>
2021-02-07 10:26:27 +0100

    Add providers to app

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+// Providers
+import Providers from './Providers';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Providers>
+      <App />
+    </Providers>
   </React.StrictMode>,
   document.getElementById('root')
 );
