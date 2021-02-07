5ac78fc527086938697d3dd73fe2f0b4ef14b7b3
Piotr Glodek <pioglodek@gmail.com>
2021-02-07 15:49:22 +0100

    Add theme module

---
+++ src/theme.d.ts
@@ -0,0 +1,93 @@
+// MUI
+import { createMuiTheme } from '@material-ui/core/styles';
+
+declare module '@material-ui/core/styles/createPalette' {
+  interface Palette {
+    pokemon: {
+      type: {
+        bug: string;
+        dark: string;
+        dragon: string;
+        electric: string;
+        fairy: string;
+        fighting: string;
+        fire: string;
+        flying: string;
+        ghost: string;
+        grass: string;
+        ground: string;
+        ice: string;
+        normal: string;
+        poison: string;
+        psychic: string;
+        rock: string;
+        steel: string;
+        water: string;
+      };
+      backgroundType: {
+        bug: string;
+        dark: string;
+        dragon: string;
+        electric: string;
+        fairy: string;
+        fighting: string;
+        fire: string;
+        flying: string;
+        ghost: string;
+        grass: string;
+        ground: string;
+        ice: string;
+        normal: string;
+        poison: string;
+        psychic: string;
+        rock: string;
+        steel: string;
+        water: string;
+      };
+    };
+  }
+  interface PaletteOptions {
+    pokemon: {
+      type: {
+        bug: string;
+        dark: string;
+        dragon: string;
+        electric: string;
+        fairy: string;
+        fighting: string;
+        fire: string;
+        flying: string;
+        ghost: string;
+        grass: string;
+        ground: string;
+        ice: string;
+        normal: string;
+        poison: string;
+        psychic: string;
+        rock: string;
+        steel: string;
+        water: string;
+      };
+      backgroundType: {
+        bug: string;
+        dark: string;
+        dragon: string;
+        electric: string;
+        fairy: string;
+        fighting: string;
+        fire: string;
+        flying: string;
+        ghost: string;
+        grass: string;
+        ground: string;
+        ice: string;
+        normal: string;
+        poison: string;
+        psychic: string;
+        rock: string;
+        steel: string;
+        water: string;
+      };
+    };
+  }
+}
