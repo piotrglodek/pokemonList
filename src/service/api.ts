2c1c715fa34d9dee49023b2818259af50ed20bb0
Piotr Glodek <pioglodek@gmail.com>
2021-02-08 16:16:18 +0100

    Delete api stastic keys

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,13 +1,9 @@
 interface API {
   url: string;
-  limit: number;
-  offset: number;
   maxResults: number;
 }
 
 export const api: API = {
   url: `https://pokeapi.co/api/v2/`,
-  limit: 9,
-  offset: 9,
   maxResults: 1118,
 };
